3e0973b91bcaa703447667a8c95114e3d34e7fa9
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2019-05-03 19:30:06 +0200

    Added 2D Sidescroller feature pack. Added Pro content.

--- UE4JamGame/Source/UE4JamGame/UE4JamGame.Build.cs
+++ UE4JamGame/Source/UE4JamGame/UE4JamGame.Build.cs
@@ -8,6 +8,6 @@ public class UE4JamGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Paper2D" });
     }
 }
